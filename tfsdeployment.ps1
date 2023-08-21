333dd7c9b0c987325c2e32c93954d9fea4c185c4
anthonyharrygit <105105439+anthonyharrygit@users.noreply.github.com>
2023-08-21 14:24:46 +0100

    Update tfsdeployment.ps1

--- tfsdeployment.ps1
+++ tfsdeployment.ps1
@@ -1,6 +1,7 @@
 #disable windows defenders Firewall 
 
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled false 
+Start-Sleep -Seconds 20
 
 #creating local user "K-TFS001RMAgent"
 $Username = "K-TFS001RMAgent"
@@ -31,10 +32,17 @@ else {
 
 Write-Host "Ensuring password for $Username never expires."
 & WMIC USERACCOUNT WHERE "Name='$Username'" SET PasswordExpires=FALSE
+Start-Sleep -Seconds 30
 
 #Add domain authentication Credentials
 
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Start-Sleep -Seconds 120
+
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Start-Sleep -Seconds 120
+
 Install-Module -Name CredentialManager
+Start-Sleep -Seconds 60
+
 New-StoredCredential -Target k-tfsbuild001.tmalan.co.uk -UserName TMA\K-TFS001RMAgent -Password "t0GO^A02^c" -Type Generic -Persist LocalMachine
